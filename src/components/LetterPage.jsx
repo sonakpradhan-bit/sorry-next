27c290224852814a16d65d11dce983d1837dfdc9
sonakpradhan-bit <sonakpradhan@gmail.com>
2025-12-02 16:45:44 +0530

    Update LetterPage.jsx

--- src/components/LetterPage.jsx
+++ src/components/LetterPage.jsx
@@ -48,7 +48,7 @@ export default function LetterPage({ setCurrentPage }) {
                 <div className="relative overflow-hidden rounded-xl bg-gradient-to-br from-pink-50 to-purple-50">
                   {/* Save the photos with name like 1.jpg, 2.jpg ... and change the src to this - /images/${i}.jpg */}
                   <img
-                    src={`/images/${i}.jpg */`}
+                    src={`/images/${i}.jpg`}
                     alt={`Beautiful memory ${i}`}
                     className="w-full h-36 object-cover"
                   />
